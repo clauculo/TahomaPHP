ec1366896481f077821213c98ef52fe689e1f168
clauculo <wouter.post@kaartje2go.nl>
2017-02-06 21:13:56 +0100

    First OO structure to build a great module.

---
+++ request.php
@@ -0,0 +1,63 @@
+<?php
+namespace tahoma;
+
+class TahomaRequest {
+
+    /**
+     * @var string $url
+     */
+    private $url = '';
+    private $domain = '';
+    private $postData = '';
+    private $ckfile = '';
+    private $createCookieJar = false;
+
+    /**
+     * tahomeRequest constructor.
+     *
+     * @param string $url starting from tld
+     * @example /enduser-mobile-web/enduserAPI/login
+     */
+    public function __construct($url, $id, $password, $ckfile='') {
+        $this->url = $url;
+        $this->domain = 'https://www.tahomalink.com';
+        $this->postData = "userId=$id&userPassword=$password";
+        $this->ckfile = $ckfile;
+        $this->createCookieJar = false;
+    }
+
+    public function execute () {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->domain . $this->url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->postData);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        if ($this->createCookieJar) {
+            curl_setopt($ch, CURLOPT_COOKIEJAR, $this->ckfile);
+        } else {
+            curl_setopt($ch, CURLOPT_COOKIEFILE, $this->ckfile);
+        }
+
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+        if ($output == "") {
+            echo $this->url . ": Invalid return\n";
+        }
+        return $output;
+    }
+
+    public function setPostData($postData) {
+        $this->postData = $postData;
+    }
+
+    public function createCookieFile() {
+        $ckfile = tempnam("/tmp", "CURLCOOKIE");
+        $this->createCookieJar = true;
+        $this->ckfile = $ckfile;
+        return $ckfile;
+    }
+}
---
+++ tahoma.php
@@ -0,0 +1,255 @@
+<?php
+require_once('request.php');
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+/**
+ * Class TahomaController
+ *
+ * A class to send commands to the Somfy Tahoma API
+ * It is now possible to create simple web-application to manage your Home automation.
+ *
+ * @author Wouter Post <wouteris@hotmail.com>
+ * @package tahoma
+ */
+class TahomaController {
+
+    /**
+     * @var string
+     */
+    private $userId;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * User is (normally an e-mailaddress)
+     *
+     * @param string $userId
+     */
+    public function setUserId($userId) {
+        $this->userId = $userId;
+    }
+
+    /**
+     * Sorry we need to pass the password unprotected over the line.
+     *
+     * @param $password
+     */
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    /**
+     * List of configurable objects, here you can debug the interfaces of your Somfy devices.
+     *
+     * @return mixed json object on success
+     */
+    public function getDevices() {
+        $url = '/enduser-mobile-web/enduserAPI/login';
+        $login = new Request($url, $this->userId, $this->password);
+        $ckfile = $login->createCookieFile();
+        $output = $login->execute();
+
+
+        $url = '/enduser-mobile-web/externalAPI/refreshAllStates';
+        $refresh = new Request($url, $this->userId, $this->password, $ckfile);
+        $output = $refresh->execute();
+
+        $url = '/enduser-mobile-web/externalAPI/json/getSetup?_=1434999539745';
+        $setup = new Request($url, $this->userId, $this->password, $ckfile);
+        $output = $setup->execute();
+
+        $tahoma = json_decode($output);
+        return $tahoma->setup->devices;
+    }
+
+    /**
+     * @param int $decode
+     * @return mixed
+     */
+    public function getScenarios($decode = 1) {
+        $url = "https://www.tahomalink.com/enduser-mobile-web/enduserAPI/login";
+        $postData = "userId=$this->userId&userPassword=$this->password";
+        $ckfile = tempnam("/tmp", "CURLCOOKIE");
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $ckfile);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        if ($output == "") {
+            echo "Invalid return";
+        }
+        $url = "https://www.tahomalink.com/enduser-mobile-web/externalAPI/json/getActionGroups";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $ckfile);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        if ($output == "") {
+            echo "Invalid return";
+        }
+        if ($decode == 0) {
+            return $output;
+        }
+        $tahoma = json_decode($output);
+        return $tahoma->actionGroups;
+    }
+
+    /**
+     * @param $execId
+     * @return string
+     */
+    public function cancelExecutions($execId) {
+        $url = "https://www.tahomalink.com/enduser-mobile-web/enduserAPI/login";
+        $postData = "userId=$this->userId&userPassword=$this->password";
+        $ckfile = tempnam("/tmp", "CURLCOOKIE");
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $ckfile);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        if ($output == "") {
+            echo "Invalid return";
+        }
+        $url = "https://www.tahomalink.com/enduser-mobile-web/externalAPI/json/cancelExecutions";
+        //	$url = "https://www.tahomalink.com/enduser-mobile-web/enduserAPI//exec/cancelExecutions";
+        log::add('tahoma', 'debug', "cancelExecutions: (" . $execId . ")");
+        $postData = ['execId' => $execId];
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $ckfile);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        log::add('tahoma', 'debug', "return http cancelExecutions: (" . $output . ")");
+        if ($output == "") {
+            echo "Invalid return";
+            return "";
+        }
+        return "";
+    }
+
+    /**
+     * Method to send commands to Tahoma api to handle action.
+     *
+     * @param $deviceURL
+     * @param $commandName
+     * @param $parameters
+     * @param string $equipmentName
+     * @return string
+     */
+    public function sendCommand($deviceURL, $commandName, $parameters, $equipmentName = "Equipment") {
+        $url = "https://www.tahomalink.com/enduser-mobile-web/enduserAPI/login";
+        $postData = "userId=$this->userId&userPassword=$this->password";
+        $ckfile = tempnam("/tmp", "CURLCOOKIE");
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $ckfile);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        if ($output == "") {
+            echo "Invalid return";
+        }
+        $url = "https://www.tahomalink.com/enduser-mobile-web/enduserAPI//exec/apply";
+        $action["deviceURL"] = $deviceURL;
+        $command["name"] = $commandName;
+        if ($parameters != "") {
+            $command["parameters"] = $parameters; // array(100);
+        }
+        $commands[] = $command;
+        $action["commands"] = $commands;
+        $actions[] = $action;
+        $row["label"] = $equipmentName;
+        $row["actions"] = $actions;
+        $postData = json_encode($row);
+        #log::add('tahoma', 'debug', $postData);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $ckfile);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        if ($output == "") {
+            echo "Invalid return";
+            return "";
+        }
+
+        $outputJson = json_decode($output);
+        return $outputJson->execId;
+    }
+
+    /**
+     * Method to execute an action
+     *
+     * @param string $oid
+     * @param int $delay
+     *
+     * @return string
+     */
+    public function execAction($oid, $delay = 0) {
+        $url = '/enduser-mobile-web/enduserAPI/login';
+        $login = new Request($url, $this->userId, $this->password);
+        $ckfile = $login->createCookieFile();
+        $output = $login->execute();
+
+        $url = sprintf("https://www.tahomalink.com/enduser-mobile-web/externalAPI/json/scheduleActionGroup?oid=%s&delay=%d", $oid, $delay);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $ckfile);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        if ($output == "") {
+            echo "Invalid return";
+            return "";
+        }
+
+        $outputJson = json_decode($output);
+        return $outputJson->actionGroup;
+    }
+}
